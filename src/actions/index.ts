584ea82fa59b8c616531f43f3b608bc7eacce578
woojinShim <cmshim22@gmail.com>
2024-06-02 20:28:05 +0900

    fifth

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,47 @@
 'use server';
 
 import {db } from '@/db'
+import { redirect } from 'next/navigation';
 
-export async function editSnippet() {
-    console.log('edit snippet called')
+export async function editSnippet(id: number, code: string) {
+    await db.snippet.update({
+        where: {id},
+        data: {code}
+    })
+
+    redirect(`/snippets/${id}`)
 }
 
+export async function deleteSnippet(id: number) {
+    await db.snippet.delete({
+        where: {id}
+    })
+    redirect('/')
+}
+
+export async function createSnippet(formState: {message: string}, formData: FormData) {
+
+    const title = formData.get('title');
+    const code = formData.get('code');        
+
+    if(typeof title !== 'string' || title.length < 3) {
+        return {
+            message: 'Title must be longer'
+        }
+    }
+    if(typeof code !== 'string' || code.length < 10) {
+        return {
+            message: 'Code must be longer'
+        }
+    }
+
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code
+        }
+    })
+    console.log(snippet)
+
+    redirect('/')
+}
--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,16 +2,21 @@
 
 import type { Snippet } from '@prisma/client'
 import Editor from '@monaco-editor/react'
-import {useState} from 'react'
+import {useState} from 'react';
+import * as actions from '@/actions';
 
 interface SnippetEditFormProps {
     snippet: Snippet
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+    const [code, setCode] = useState(snippet.code)
     const handleEditorChange = (value: string = "") => {
-        console.log(value)
+        setCode(value)
     }
+
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
+
     return (<div>
         <Editor 
             height='40vh'
@@ -21,5 +26,8 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
             options={{ minimap: {enabled: false} }}
             onChange={handleEditorChange}
         />
+        <form action={editSnippetAction}>
+            <button type="submit" className='p-2 border rounded'>Save</button>
+        </form>
     </div>)
 }
